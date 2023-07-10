ca3a58fe067699ad7cb07bc84629e05e8e6e8369
eldho <eldhomathew796@gmail.com>
2023-07-10 03:33:26 -0700

    first-commit

--- onlinemarket/settings.py
+++ onlinemarket/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'onlinemarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlineshoppingproduct',
+        'USER':'root',
+        'HOST':'localhost',
+        'PASSWORD':'',
+        'PORT':'3306',
     }
 }
 
